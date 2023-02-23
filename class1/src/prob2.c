afa01f4f2d2857afaeae06e50359446f981ed20d
Dorde Zivanovic <popina1994@users.noreply.github.com>
2023-02-23 13:12:27 +0000

    Update prob2.c

--- class1/src/prob2.c
+++ class1/src/prob2.c
@@ -4,10 +4,10 @@
 char* binaryInt(int value)
 {
     int len = sizeof(value) * 8;
-    char* binStr = malloc(len + 1);
+    char* binStr = (char*)malloc(len + 1);
     for (int idx = len - 1; idx >= 0; idx --)
     {
-        int testBit = 1 << (idx - 1);
+        int testBit = 1 << idx;
         char cOut;
         if (value & testBit)
         {
@@ -20,7 +20,7 @@ char* binaryInt(int value)
         binStr[len - idx - 1] = cOut;
         value &= ~(testBit);
     }
-    binStr[len] = 0; 
+    binStr[len] = '\0'; 
     return binStr;
 }
 
@@ -31,5 +31,9 @@ int main()
     cOut = binaryInt(25);
     printf("Value is: %s\n", cOut);
     cOut = binaryInt(1111);
+    printf("Value is: %s\n", cOut);
+    cOut = binaryInt(-1);
+    printf("Value is: %s\n", cOut);
+    cOut = binaryInt(-3);
     printf("Value is: %s\n", cOut);
 }
